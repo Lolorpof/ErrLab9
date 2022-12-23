e120b1fa3237164be1fa3357146b63b6d33425d8
Lolorpof <sirawit_sir@cmu.ac.th>
2022-12-23 15:37:10 +0700

    lab9_3 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,34 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age, height, bounty;
+    string chara;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100) chara = "Chopper";
+        else if(height < 180) chara = "Usopp";
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 1100000000) chara = "Zoro";
+            else chara = "Sanji";
+        }
+    }
+    else if(age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 500000000) chara = "Jinbe";
+        else chara = "Franky";
+    }
+    else{
+        chara = "Brook";
+    }
+
+    cout << "Your character = " << chara;
+
+    return 0;
+}
